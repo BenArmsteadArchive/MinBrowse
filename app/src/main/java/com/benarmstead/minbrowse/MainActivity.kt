eaadba1eb582c1ca0d5c72ea2dfeeae035cd956e
Ben Armstead <benarmstead@protonmail.com>
2021-07-26 19:32:07 +0100

    Create web view

--- app/src/main/java/com/benarmstead/minbrowse/MainActivity.kt
+++ app/src/main/java/com/benarmstead/minbrowse/MainActivity.kt
@@ -2,10 +2,18 @@ package com.benarmstead.minbrowse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val view = findViewById<WebView>(R.id.webView)
+        view.webViewClient = WebViewClient()
+        view.loadUrl("https://www.duckduckgo.com")
+        val settings = view.settings
+        settings.javaScriptEnabled = true
     }
 }
