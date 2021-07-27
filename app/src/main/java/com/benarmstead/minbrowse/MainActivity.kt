4775813038fa1184db1ed9a01bb9de96b0bf784b
Ben Armstead <benarmstead@protonmail.com>
2021-07-27 10:53:25 +0100

    Make android back button go back 1 page

--- app/src/main/java/com/benarmstead/minbrowse/MainActivity.kt
+++ app/src/main/java/com/benarmstead/minbrowse/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.benarmstead.minbrowse
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.CookieManager
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var view: WebView
@@ -35,6 +35,14 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.toggleJS).setOnClickListener{
             toggleJS()
+        }
+    }
+
+    override fun onBackPressed() {
+        if (view.canGoBack()){
+            view.goBack()
+        }else{
+            super.onBackPressed()
         }
     }
 
